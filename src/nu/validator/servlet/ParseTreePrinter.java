b7fca93aeb613ae7bdaff3ed6844fe65484887a5
prushforth <peter.rushforth@canada.ca>
2019-02-01 13:11:29 -0500

    Update ParseTreePrinter to let text/mapml pass through the HTML parser. Change the default / dummy document from html to mapml in the textarea of the validator form.

--- src/nu/validator/servlet/ParseTreePrinter.java
+++ src/nu/validator/servlet/ParseTreePrinter.java
@@ -143,7 +143,7 @@ public class ParseTreePrinter {
             }
             String type = documentInput.getType();
             XMLReader parser;
-            if ("text/html".equals(type) || "text/html-sandboxed".equals(type)) {
+            if ("text/mapml".equals(type)|| "text/html".equals(type) || "text/html-sandboxed".equals(type)) {
                 writer.write("HTML parser\n\n#document\n");
                 parser = new nu.validator.htmlparser.sax.HtmlParser();
                 parser.setProperty("http://validator.nu/properties/heuristics", Heuristics.ALL);
